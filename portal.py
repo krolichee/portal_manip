0f278cf82ac32bffa3b4cecbbdbe5127588099c1
krolichee <digital.homyak@gmail.com>
2025-02-07 19:46:35 +0800

    ...

--- portal.py
+++ portal.py
@@ -47,10 +47,8 @@ m_y.run_to_rel_pos(position_sp=Y_AMP_MAX/2)
 
 while True:
     inp = input()
-    if inp == "w":
-        m_y.run_to_rel_pos(position_sp=100)
-    elif inp == "s":
-        m_y.run_to_rel_pos(position_sp=-100)
+    print(inp)
+    m_y.run_to_rel_pos(position_sp=-100)
 
 
 
