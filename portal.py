1256cb78e6821d6a8b73d819b51574eacf370d86
krolichee <digital.homyak@gmail.com>
2025-02-07 19:21:07 +0800

    ...

--- portal.py
+++ portal.py
@@ -13,32 +13,38 @@ m_y = MediumMotor(OUTPUT_A)
 m_x1 = Motor(OUTPUT_B)
 m_x2 = LargeMotor(OUTPUT_D)
 mt_x = MoveTank(OUTPUT_B, OUTPUT_D)
+mt_x.cs = ColorSensor(INPUT_2)
 
-m_y.run_forever(speed_sp=-300)
-ts_y.wait_for_pressed()
-m_y.stop()
+def home_y():
+    m_y.run_forever(speed_sp=-300)
+    ts_y.wait_for_pressed()
+    m_y.stop()
 
+
+
+
+
+def home_x():
+    mt_x.run_forever(speed_sp=-300)
+    ts_x.wait_for_pressed()
+    mt_x.stop()
+
+
+def test_x():
+    mt_x.run_forever(speed_sp=100)
+    ts_x.wait_for_released()
+
+    try:
+        while not ts_x.is_pressed:
+            mt_x.follow_line( kp=11.3, ki=0.05, kd=3.2, speed=SpeedPercent(30),
+                              follow_for=follow_for_forever,follow_left_edge=False,target_light_intensity=16,sleep_time=0.01,)
+    except LineFollowErrorTooFast:
+        mt_x.stop()
+    mt_x.stop()
+
+
+home_y()
 m_y.speed_sp = 500
 m_y.run_to_rel_pos(position_sp=Y_AMP_MAX/2)
 
-
-
-mt_x.cs = ColorSensor(INPUT_2)
-
-mt_x.run_forever(speed_sp=-300)
-ts_x.wait_for_pressed()
-mt_x.stop()
-
-
-
-mt_x.run_forever(speed_sp=100)
-ts_x.wait_for_released()
-
-try:
-    while not ts_x.is_pressed:
-        mt_x.follow_line( kp=11.3, ki=0.05, kd=3.2, speed=SpeedPercent(30),
-                          follow_for=follow_for_forever,follow_left_edge=False,target_light_intensity=16,sleep_time=0.01,)
-except LineFollowErrorTooFast:
-    mt_x.stop()
-mt_x.stop()
 
